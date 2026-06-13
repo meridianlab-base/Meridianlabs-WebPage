4033f1931ab94836ade529770a223aa87864889d
MeridianLabs <meridianlabs.tech@gmail.com>
2026-06-14 01:25:42 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build
 export default defineConfig({
-  site: "https://meridianlab-base.github.io",
-  base: "/Meridianlabs-WebPage",
+  // site: "https://meridianlab-base.github.io",
+  // base: "/Meridianlabs-WebPage",
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
